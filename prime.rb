3f2c375e835a094dbf147aedf2c3198cd8f19625
David masterson <david73180@gmail.com>
2018-09-18 04:27:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,13 @@
 
 def prime?(num)
   case num
-    when % 1..10 == 0
+    when % 2 == 0
+      return false
+    when % 3 == 0 
+      return false
+    when % 4 == 0 
+      return false
+    when % 5 == 0 
       return false
   end  
 end
