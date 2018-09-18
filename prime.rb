f654030b63b61cbe257ca3adf2c982ca285dc409
David masterson <david73180@gmail.com>
2018-09-18 03:11:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime(range)
   number.each do 
     array.push(number)
   end
- for (x = 0, x < array.length, x++) 
+ for x = 0, x < array.length, x++
   if array[x]== Math.prime
     return true
   else
