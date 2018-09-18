18d635e034f638e47c2a1ae40a64954f2f98c939
David masterson <david73180@gmail.com>
2018-09-18 03:55:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,17 @@
 def prime?(num)
+ array = []
+ range = (1..10) 
+ range.each do|x|
+ array.push(x)
+ end
+ y = 0 
+ until y > array.length 
+ array.each do |number|
+   if (num / 1 == num && num / num == 1) && num % 2..10 != 0
+     return true 
+   else
+     return false 
+   end
+   y+=1
+ end
 end
