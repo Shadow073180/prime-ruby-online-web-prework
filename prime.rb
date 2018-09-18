ddcfecb3b9792a020592a71fc8529d8795052dcb
David masterson <david73180@gmail.com>
2018-09-18 03:58:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,7 @@ def prime?(num)
  y = 0 
  until y > array.length 
  array.each do |number|
-   if (num / 1 == num && num / num == 1) && (num % 2||3||4||5||6||7||8||9||10  != 0)
+   if (num / 1 == num && num / num == 1) && (num % 2||3||4||5||6||7||8||9||10  != 0)&& num > 0
      return true 
    else
      return false 
