1836902a6dac83bbc9d5f32b669258b376189b52
David masterson <david73180@gmail.com>
2018-09-18 04:16:20 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,4 @@
 def prime?(num)
- array = []
- range = (1..10) 
- range.each do|x|
- array.push(x)
- end
- array.each do|y|
-   if y/y==1 && y/1==y 
-     return true
-   elsif y<0 == 'ZeroDivisionError'
-     return false
-   end
- end
+ prime_flag = true 
+ 
 end
