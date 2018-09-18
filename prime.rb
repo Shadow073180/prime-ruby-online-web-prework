bf4c8c2d3168817ac33ead23d0a26185b00c5535
David masterson <david73180@gmail.com>
2018-09-18 04:02:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,15 +4,4 @@ def prime?(num)
  range.each do|x|
  array.push(x)
  end
- y = 0 
- until y > array.length 
- array.each do |number|
-   if (num / 1 == num && num / num == 1) && (num % 2||3||4||5||6||7||8||9||10  != 0)&& num > 0 && != ZeroDivisionError
-     return true 
-   else
-     return false 
-   end
-   y+=1
- end
-end
-end
+
